3519cecfd36deca74b2148bc8960d8a33dffb4cf
robert3827 <32315287+robert3827@users.noreply.github.com>
2023-12-05 15:42:58 -0600

    add map and function to compute distance to home address. We may want to move the latter.

--- GCache/app/src/main/java/com/example/gcache/MapsActivity.java
+++ GCache/app/src/main/java/com/example/gcache/MapsActivity.java
@@ -1,9 +1,20 @@
 package com.example.gcache;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,11 +22,27 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.gcache.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private static String[] DESIRED_PERMISSIONS = {android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+
+    Geocoder geocoder;
+    Geocoder.GeocodeListener geocodeListener;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    private Location curLocation;
+    private String homeString;
+    private Location homeLocation;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +55,87 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        geocoder = new Geocoder(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getCurrentLocation();
     }
+    private void getCurrentLocation() {
+        /**
+         * Check if I have permission to access COARSE/FINE Location
+         */
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            /**
+             * I don't have permissions so request Permissions
+             */
+            ActivityCompat.requestPermissions(this, DESIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
+            getCurrentLocation();
+        } else {
+
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                LatLng curLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().position(curLatLng).title("CurrentLocation"));
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(curLatLng));
+                                curLocation = location;
+                            }
+                        }
+
+                    });
+        }
+    }
+    private void distFromHome() {
+        int distFromHome = 0;
+        geocodeListener = new Geocoder.GeocodeListener() {
+            @Override
+            public void onGeocode(@NonNull List<Address> list) {
+                Address a = list.get(0);
+                Log.d(TAG, "Location of " + homeString);
+                homeLocation = new Location("Destination");
+                homeLocation.setLatitude(a.getLatitude());
+                homeLocation.setLongitude(a.getLongitude());
+
+
+                Log.d(TAG, "onGeocode: Lat: " +
+                        homeLocation.getLatitude() + " LNG: " + homeLocation.getLongitude());
+
+                /**
+                 * Display the distance to my goal.
+                 */
+                displayDistance(curLocation, homeLocation);
+
+
+
+
+            }
+            private void displayDistance(Location l1, Location l2) {
+                float distanceMeters;
+                float distanceKm;
+                float[] results = new float[1];
+                Location.distanceBetween(curLocation.getLatitude(), curLocation.getLongitude(), homeLocation.getLatitude(), homeLocation.getLongitude(), results);
+                distanceMeters = results[0];
+                distanceKm = distanceMeters/1000;
+                Log.d(TAG, "displayDistance: Distance between: " + distanceKm + " meters");
+
+            }
+        };
+        /**
+         * Implememnt their listener
+         */
+        geocoder.getFromLocationName(homeString,1, geocodeListener);}
+
+
 }
