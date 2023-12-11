8368cffb505baa86bce0dba90ea45ff6a4cda800
samanthaneri <samneri@iastate.edu>
2023-12-10 20:27:52 -0600

    login page java doc

--- GCache/app/src/main/java/com/example/gcache/LoginActivity.java
+++ GCache/app/src/main/java/com/example/gcache/LoginActivity.java
@@ -37,8 +37,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
 
     /**
-     *
-     * @param savedInstanceState
+     * Sets login page and its variables, to include edit texts for email and password
+     * @param savedInstanceState Saved instance of login page
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +52,10 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.loginActivity_editText_password);
     }
 
+    /**
+     * Checks if the user is signed into their account or created a new account
+     * Updates the UI accordingly
+     */
     @Override
     public void onStart() {
         super.onStart();
@@ -63,6 +67,10 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Allows the user to input their email and password to sign in
+     * @param view Sign in view
+     */
     public void onSignInClicked(View view) {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -70,6 +78,10 @@ public class LoginActivity extends AppCompatActivity {
         signIn(email, password);
     }
 
+    /**
+     * Allows the user to input an email and password to sign up
+     * @param view Sign up view
+     */
     public void onSignUpClicked(View view) {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -77,6 +89,13 @@ public class LoginActivity extends AppCompatActivity {
         signUp(email, password);
     }
 
+    /**
+     * Check sign in parameters to include email and password.
+     * If successful, UI is updated with signed-in user's info
+     * If the sign in fails, error message is displayed to user
+     * @param email String provided by user, which is a valid email
+     * @param password String provided by user, which is a valid password
+     */
     private void signIn(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -97,6 +116,13 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
+    /**
+     * Checks sign up parameters,to include user email and password
+     * If sign up is successful, UI is updated with the user's information and adds the user to Firebase
+     * If failed, error message displayed to user
+     * @param email string, which is user's valid email
+     * @param password string, which is user's valid password
+     */
     private void signUp(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
